7eef6e88733ffc8d0dbc61b611469a2925f32f60
maximemlly <maxime.maillary@icloud.com>
2026-03-12 13:55:29 +0100

    project structure

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.pokemonjavafx {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.pokemonjavafx to javafx.fxml;
-    exports com.example.pokemonjavafx;
-}
