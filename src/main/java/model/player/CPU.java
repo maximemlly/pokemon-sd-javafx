02b90e493e5697fe62edfefb11fbe73cf3f51f6d
Emery P. <emerypakula@gmail.com>
2026-03-13 11:52:27 +0100

    cpu draft

--- src/main/java/model/player/CPU.java
+++ src/main/java/model/player/CPU.java
@@ -1,4 +1,29 @@
 package model.player;
 
 public class CPU {
+
+    /*
+    DefensiveStrategy() {
+        if (CPU pokemon has move where effect is on self (heal, shield)) {
+            CPU pokemon use move where effect is on self (heal, shield)
+        }
+        else {
+            randomly select a move from CPU pokemon move list
+            return selected move
+        }
+    }
+
+    OffensiveStrategy() {
+        if (CPU pokemon has move where type is super effective against opponent pokemon type) {
+            CPU pokemon use move where type is super effective against opponent pokemon type
+        }
+        else if (CPU pokemon has any attack move) {
+            CPU pokemon use random attack move
+        }
+        else {
+            randomly select a move from CPU pokemon move list
+            return selected move
+        }
+    }
+    */
 }
